8df52b5ba56bc78c2087e8c457639803aa8ec42c
Godelvatai <godelvatai@gmail.com>
2024-03-25 01:12:34 -0400

    web 340 assignment 1.3
    homework push

--- week-1/weight-converter/weight-converter.js
+++ week-1/weight-converter/weight-converter.js
@@ -1,10 +1,30 @@
 /**
- * Author:
- * Date:
- * File Name:
- * Description:
+ * Author:      Dustin Craven
+ * Date:        3/24/24
+ * File Name:   weight-converter.js
+ * Description: Node.js script that converts pounds to kilograms
 */
 
 "use strict";
+const { argv } = require("node:process");
 
 // TODO: Implement the weight conversion logic here
+function main() {
+  if(process.argv[2]){
+    let regex = /^[0-9]/;
+    if(process.argv[2].match(regex)) {
+      let pounds = process.argv[2];
+      let kilograms =  (pounds/2.205).toFixed(2);
+      console.log(pounds+" pounds is "+kilograms+" kilograms.");
+    } else {
+      console.error("Input must be a number");
+      process.exit(1);
+    }
+  } else {
+    console.error("Usage: node weight-converter.js <pounds>");
+    process.exit(1);
+  }
+}
+
+//call main function
+main();
