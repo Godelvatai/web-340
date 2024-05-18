caf3df228e0247ca601fcee67da35752411856eb
Godelvatai <godelvatai@gmail.com>
2024-05-18 02:42:50 -0400

    Week 6 HW
    Week 6 HW

--- week-6/fantasy-game-character-creation/src/server.js
+++ week-6/fantasy-game-character-creation/src/server.js
@@ -1,10 +1,48 @@
+/**
+ * Author:      Dustin Craven
+ * Date:        5/16/24
+ * File Name:   server.js
+ * Description: Server and routes for a character creation application.
+ */
+"use strict";
+
 const http = require('http');
 const url = require('url');
 
-// TODO: Implement your server here
-
 const server = http.createServer((req, res) => {
-  // TODO: Implement your routes here
+  //Parse url and query and get the path
+  const parsedUrl = url.parse(req.url, true);
+  const pathname = parsedUrl.pathname;
+  const query = parsedUrl.query;
+
+  // Creates character object to store character data
+  let character = {
+    characterClass: '',
+    characterGender: '',
+    characterFunFact: ''
+  };
+
+  if(pathname === "/createcharacter" && req.method === "POST"){
+    //Set HTTP code for successful response
+    res.writeHead(201);
+
+    character.characterClass = query.class;
+    character.characterGender = query.gender;
+    character.characterFunFact = query.fact;
+
+    res.end(`Character created successfully.`);
+  } else if(pathname === "/confirm" && req.method === "POST") {
+    //Set HTTP code for successful response
+    res.writeHead(200);
+    res.end(`Character has been created.`);
+  } else if(pathname === "/viewcharacter" && req.method === "GET") {
+    //Set HTTP code for successful response
+    res.writeHead(200);
+    res.end(`Character Class: ${character.characterClass}\nCharacter Gender: ${character.characterGender}\nCharacter Fun Fact: ${character.characterFunFact}`);
+  } else {
+    res.writeHead(404);
+    res.end();
+  }
 });
 
 server.listen(3000, () => {
--- week-6/fantasy-game-character-creation/test/server.spec.js
+++ week-6/fantasy-game-character-creation/test/server.spec.js
@@ -1,4 +1,77 @@
+/**
+ * Author:      Dustin Craven
+ * Date:        5/16/24
+ * File Name:   server.spec.js
+ * Description: Test for server.js
+ */
+"use strict";
+
 const http = require('http');
 const server = require('../src/server');
 
 // TODO: Implement your tests here
+describe('server', () => {
+  //Close server after each test
+  afterAll(() => {
+    server.close();
+  });
+
+  //Test case for /createcharacter POST
+  test("Test /createcharacter POST", done => {
+    const options = {
+      hostname: 'localhost',
+      port: 3000,
+      path: '/createcharacter?class=Mage&gender=Male&fact=I+only+invested+in+lightning+magic',
+      method: 'POST'
+    };
+    const req = http.request(options, res => {
+      let data = '';
+      res.on('data', chunk => {
+        data += chunk;
+      });
+      res.on('end', () => {
+        expect(res.statusCode).toBe(201);
+        expect(data).toBe('Character created successfully.');
+        done();
+      });
+    });
+    req.end();
+  });
+
+  //Test /viewcharacter GET to view created character
+  test("Test /viewcharacter GET if character has been created", done => {
+    http.get('http://localhost:3000/viewcharacter', res => {
+      let data = '';
+      res.on('data', chunk => {
+        data += chunk;
+      });
+      res.on('end', () => {
+        expect(res.statusCode).toBe(200);
+        expect(data).toBe('Character Class: \nCharacter Gender: \nCharacter Fun Fact: ');
+        done();
+      });
+    });
+  });
+
+  //Test /confirm POST to confirm if character was created
+  test("Test /confirm POST if character has been created", done => {
+    const options = {
+      hostname: 'localhost',
+      port: 3000,
+      path: '/confirm',
+      method: 'POST'
+    };
+    const req = http.request(options, res => {
+      let data = '';
+      res.on('data', chunk => {
+        data += chunk;
+      });
+      res.on('end', () => {
+        expect(res.statusCode).toBe(200);
+        expect(data).toBe('Character has been created.');
+        done();
+      });
+    });
+    req.end();
+  });
+});
