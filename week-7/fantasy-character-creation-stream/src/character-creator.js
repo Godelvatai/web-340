254a2b6c704ab8fdfb3c53f8f7e4c1d69d67f8f1
Godelvatai <godelvatai@gmail.com>
2024-05-18 22:43:10 -0400

    Week 7 HW Push
    Week 7 HW Push

--- week-7/fantasy-character-creation-stream/src/character-creator.js
+++ week-7/fantasy-character-creation-stream/src/character-creator.js
@@ -1,17 +1,46 @@
+/**
+ * Author:      Dustin Craven
+ * Date:        5/18/24
+ * File Name:   character-creator.js
+ * Description: .
+ */
+"use strict";
+
 const { Duplex } = require('stream');
 
 class CharacterCreator extends Duplex {
   constructor(options) {
     super(options);
-    // TODO: Initialize your class here
+    this.queue = [];
   }
 
   _write(chunk, encoding, callback) {
-    // TODO: Implement your _write method here
+    //Convert character data to a string
+    const characterInput = chunk.toString();
+
+    if(!characterInput) {
+      //Error for invalid input for character data
+      callback(new Error('Invalid character data'));
+
+      //Return method to stop code from continuing
+      return;
+    }
+
+    //Take string input of object and convert it back to an object for the formatted output
+    let characterObj = JSON.parse(characterInput);
+
+    //Formatted description of character data
+    this.queue.push(`--Character Data--\nClass: ${characterObj.characterClass}\nGender: ${characterObj.characterGender}\nFun Fact: ${characterObj.characterFact}`);
+    callback();
   }
 
   _read(size) {
     // TODO: Implement your _read method here
+    if(this.queue.length) {
+      this.push(this.queue.shift() + "\n");
+    } else {
+      this.push(null);
+    }
   }
 }
 
--- week-7/fantasy-character-creation-stream/test/character-creator.spec.js
+++ week-7/fantasy-character-creation-stream/test/character-creator.spec.js
@@ -1,21 +1,73 @@
+/**
+ * Author:      Dustin Craven
+ * Date:        5/18/24
+ * File Name:   character-creator.spec.js
+ * Description: Test file for character-creator.js.
+ */
+"use strict";
+
 const CharacterCreator = require('../src/character-creator');
 
 describe('CharacterCreator', () => {
   let characterCreator;
+
+  //Objects for test characters
+  let characterTest1 = {
+    characterClass: "Mage",
+    characterGender: "Male",
+    characterFact: "I create random effects when I cast magic sometimes."
+  };
+  let characterTest2 = {
+    characterClass: "Warrior",
+    characterGender: "Female",
+    characterFact: "I can lift an entire wagon with the horses still attached."
+  };
+  let characterTest3 = {
+    characterClass: "Rouge",
+    characterGender: "Other",
+    characterFact: "I can pick a lock with one hand."
+  };
+
+  //The expected output for each test charcter
+  let expectedOutputTest1 = "--Character Data--\nClass: Mage\nGender: Male\nFun Fact: I create random effects when I cast magic sometimes.";
+  let expectedOutputTest2 = "--Character Data--\nClass: Warrior\nGender: Female\nFun Fact: I can lift an entire wagon with the horses still attached.";
+  let expectedOutputTest3 = "--Character Data--\nClass: Rouge\nGender: Other\nFun Fact: I can pick a lock with one hand.";
 
   beforeEach(() => {
     characterCreator = new CharacterCreator();
   });
 
   test("should process data correctly when written to", (done) => {
-    // TODO: Write your test here
+    //Create array of test characters
+    const characters = [characterTest1, characterTest2, characterTest3];
+
+    //Convert object to string for input with JSON.stringify for each character of array
+    characters.forEach(character => characterCreator.write(JSON.stringify(character)));
+
+    characterCreator.on('data', (data) => {
+      const character = characters.shift();
+      expect(data.toString().trim()).toBe(`--Character Data--\nClass: ${character.characterClass}\nGender: ${character.characterGender}\nFun Fact: ${character.characterFact}`);
+      if(characters.length === 0) done();
+    });
   });
 
   test("should emit 'error' when invalid data is written", (done) => {
-    // TODO: Write your test here
+    //Check for error message for invalid data
+    characterCreator.on('error', (err) => {
+      expect(err.message).toBe("Invalid character data");
+      done();
+    });
+
+    characterCreator.write("");
   });
 
   test("should transform data correctly when written to", (done) => {
-    // TODO: Write your test here
+    //Convert object to string for input with JSON.stringify
+    characterCreator.write(JSON.stringify(characterTest1), 'utf8', () => {
+      characterCreator.on('data', (data) => {
+        expect(data.toString().trim()).toBe(expectedOutputTest1);
+        done();
+      });
+    });
   });
 });
